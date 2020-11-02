719542d0723976b8fa656edf7c67badac40f6882
unknown <migueleyen@gmail.com>
2020-11-02 18:43:23 -0500

    Update MongoURI URLDB Local or Remote Cloud

--- server/config/config.js
+++ server/config/config.js
@@ -4,16 +4,30 @@ process.env.PORT=process.env.PORT || 3000;
 
 // Base de Datos
 
-let urlDB='';
+// let urlDB='';
 
 // urlDB='mongodb://localhost:27017/Coffee';
 
-urlDB='mongodb+srv://dbMiguel:uel8dTGYJ9zLylCD@cluster0.hpcrk.mongodb.net/Coffee';
+// urlDB='mongodb+srv://dbMiguel:uel8dTGYJ9zLylCD@cluster0.hpcrk.mongodb.net/Coffee';
 
-process.env.MongoURI=urlDB;
+// process.env.MongoURI=urlDB;
 
-console.log(process.env.MongoURI);
+// console.log(process.env.MongoURI);
 
+process.env.NODE_ENV=process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if(process.env.NODE_ENV==='dev'){
+
+    //Local MongoDB 
+    urlDB='mongodb://localhost:27017/Coffee';
+}else{
+    //Remote MongoDB Cloud
+    urlDB='mongodb+srv://dbMiguel:uel8dTGYJ9zLylCD@cluster0.hpcrk.mongodb.net/Coffee';
+}
+
+process.env.MONGOURI=urlDB;
 
 
 
